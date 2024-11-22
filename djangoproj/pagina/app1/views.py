10e0d62b5085ac36ef7a9f81b8adf8847b5ae6cb
godofcode22 <demollinedo@gmail.com>
2024-11-22 14:00:47 -0600

    validación del servidor

--- djangoproj/pagina/app1/views.py
+++ djangoproj/pagina/app1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def hola(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello world!</h1>")
 
 # Create your views here.
