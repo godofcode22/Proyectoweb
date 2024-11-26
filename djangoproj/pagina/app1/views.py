83a3e42e619da55b2c3aa823d622ae3aff47553c
godofcode22 <demollinedo@gmail.com>
2024-11-26 10:52:01 -0600

    creacion de extensiones

--- djangoproj/pagina/app1/views.py
+++ djangoproj/pagina/app1/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def hola(request):
     return HttpResponse("Hello world!")
+def about(request):
+    return HttpResponse("<h1>About</h1>")
 
 # Create your views here.
