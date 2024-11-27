c91b7591139e3475e89fab1d748dca7e873e9df0
godofcode22 <demollinedo@gmail.com>
2024-11-27 16:14:28 -0600

    database set

--- djangoproj/pagina/app1/models.py
+++ djangoproj/pagina/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class proyect(models.Model):
+    name= models.CharField(max_length=200)
 
-# Create your models here.
+class trajes(models.Model):
+  title= models.CharField(max_length=150)
+  desc= models.TextField()
+  project=models.ForeignKey(proyect,on_delete=models.CASCADE)
+
