7f88060d4257c681eb51bc632def14190b16176a
godofcode22 <demollinedo@gmail.com>
2024-12-02 10:00:36 -0600

    fotos

--- djangoproj/pagina/app1/views.py
+++ djangoproj/pagina/app1/views.py
@@ -6,11 +6,14 @@ def hola(request):
     return HttpResponse("<h1>Hello world!</h1>")
 
 def about(request):
-    imagen_url = static('descarga.jpeg')
+    imagen_url = static('sour.jpeg')
+    
     print(imagen_url)
     html=(f"""
-        <h1>About</h1>
-        <img src="{imagen_url}" alt="Ejemplo de Imagen" style="width: 300px;">
+        <center>
+        <h1><font color='c209d8'>I KNOW YOU GET DEJA VU!</font></h1>
+        <img src="{imagen_url}" alt="Ejemplo de Imagen" style="width: 300px; height:300px">
+        </center>
     """)
     return HttpResponse(html)
 # Create your views here.
